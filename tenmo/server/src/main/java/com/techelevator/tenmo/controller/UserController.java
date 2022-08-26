22bb0c6f83d2c860f85a1ec3c6197873f3da25ba
Joe Ross <ross3jh@gmail.com>
2022-08-26 15:56:50 -0400

    UserController authentication enforced and passwords are no longer returned to the client

--- tenmo/server/src/main/java/com/techelevator/tenmo/controller/UserController.java
+++ tenmo/server/src/main/java/com/techelevator/tenmo/controller/UserController.java
@@ -2,11 +2,14 @@ package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.business.UserBusiness;
 import com.techelevator.tenmo.model.User;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -17,8 +20,14 @@ public class UserController {
         this.userBusiness = userBusiness;
     }
 
+    @PreAuthorize("isAuthenticated()")
     @RequestMapping(path = "/users", method = RequestMethod.GET)
-    public List<User> getAllUsers() {
-        return userBusiness.getAllUsers();
+    public Map<Integer, String> getAllUsers() {
+        List<User> userList = userBusiness.getAllUsers();
+        Map<Integer, String> userIdName = new HashMap<>();
+        for (User user : userList) {
+            userIdName.put(user.getId(), user.getUsername());
+        }
+        return userIdName;
     }
 }
